2cfce64746af8725cbe04a09f3357e59c855ca4b
Nayanjyoti Rabha <njrabha6@gmail.com>
2022-07-04 14:49:26 +0530

    new commit

--- src/main/java/com/main/GetSolutions.java
+++ src/main/java/com/main/GetSolutions.java
@@ -14,19 +14,20 @@ public class GetSolutions extends Base{
 		setBrowser("chrome", "https://stackoverflow.com");  
 		driver.findElement(By.xpath("/html/body/header/div/form/div/input")).sendKeys("java.lang.ArrayIndexOutOfBoundsException" + Keys.ENTER);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("/html/body/div[5]/div/button[1]")).click();
+		driver.findElement(By.xpath("/html/body/div[4]/div/button[1]")).click();
 		Thread.sleep(2000);
 		
 		Answers.add(Header);
+		
 		for(int i = 1; i <= 10 ; i++) {
 			Heading = driver.findElement(By
-					.xpath("/html/body/div[4]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[2]/h3/a"));
+					.xpath("/html/body/div[3]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[2]/h3/a"));
 			Answer = driver.findElement(By
-					.xpath("/html/body/div[4]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[2]/div[1]"));
+					.xpath("/html/body/div[3]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[2]/div[1]"));
 			Views = driver.findElement(By
-					.xpath("/html/body/div[4]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[1]/div[3]/span[1]"));
+					.xpath("/html/body/div[3]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[1]/div[3]/span[1]"));
 			Votes = driver.findElement(By
-					.xpath("/html/body/div[4]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[1]/div[1]/span[1]"));
+					.xpath("/html/body/div[3]/div[2]/div[1]/div[4]/div/div[" + i + "]/div[1]/div[1]/span[1]"));
 			
 			Answers.add(new String[] {
 					Heading.getText(),
@@ -36,14 +37,12 @@ public class GetSolutions extends Base{
 			}); 
 			
 		}
-		
-		//Heading
-		///html/body/div[3]/div[2]/div[1]/div[4]/div/div[1]/div[2]/h3/a
-		///html/body/div[3]/div[2]/div[1]/div[4]/div/div[2]/div[2]/h3/a
+
 
 		writernew = new CSVWriter(new FileWriter("data.csv"));
 		writernew.writeAll(Answers);
 		writernew.close();
+		System.out.println("File writen to the data.csv file successfully");
 		
 		
 	}
